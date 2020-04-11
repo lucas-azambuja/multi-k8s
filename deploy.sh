bdedf7b3d91e6a29804d2d62814f8bccf406dcbd
Lucas Azambuja <lucas.azambuja@foregon.com>
2020-04-11 00:02:40 -0300

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t lucasazambuja/multi-docker-client:latest -t lucasazambuja/multi-docker-client:$SHA -f ./client/Dockerfile ./client
+docker build -t lucasazambuja/multi-docker-server:latest -t lucasazambuja/multi-docker-server:$SHA -f ./server/Dockerfile ./server
+docker build -t lucasazambuja/multi-docker-worker:latest -t lucasazambuja/multi-docker-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push lucasazambuja/multi-docker-client:latest
+docker push lucasazambuja/multi-docker-client:$SHA
+docker push lucasazambuja/multi-docker-server:latest
+docker push lucasazambuja/multi-docker-server:$SHA
+docker push lucasazambuja/multi-docker-worker:latest
+docker push lucasazambuja/multi-docker-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=lucasazambuja/multi-docker-server:$SHA
+kubectl set image deployments/server-deployment client=lucasazambuja/multi-docker-client:$SHA
+kubectl set image deployments/server-deployment worker=lucasazambuja/multi-docker-worker:$SHA
